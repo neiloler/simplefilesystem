68693c9137ac7883b1b04f4c5de54a69766f0303
Neil Oler <neiloler@gmail.com>
2015-04-13 17:05:39 -0600

    First draft of simple command parser.

--- src/neiloler/filesystem/FileSystemEngine.java
+++ src/neiloler/filesystem/FileSystemEngine.java
@@ -1,10 +1,29 @@
 package neiloler.filesystem;
 
+import java.util.Scanner;
+
 public class FileSystemEngine {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		boolean runEngine = true;
+		Scanner input = new Scanner(System.in);
+		
+		while(runEngine) {
+			System.out.print("Command: ");
+			
+			String command = input.nextLine();
+			
+			System.out.println("COMMAND: " + command);
+			
+			// Parse command
+			if (command.equals("exit")) {
+				runEngine = false;
+			}
+		}
+		
+		input.close();
+		System.out.println("EXITING");
 	}
 
 }
