2e199d9e6fff37f8bb8691655d55af103ece6af8
Neil Oler <neiloler@gmail.com>
2015-04-14 09:23:07 -0600

    Get set of command pieces.

--- src/neiloler/filesystem/FileSystemEngine.java
+++ src/neiloler/filesystem/FileSystemEngine.java
@@ -9,21 +9,37 @@ public class FileSystemEngine {
 		boolean runEngine = true;
 		Scanner input = new Scanner(System.in);
 		
+		FileSystemController controller = new FileSystemController();
+		
 		while(runEngine) {
 			System.out.print("Command: ");
 			
-			String command = input.nextLine();
+			String[] command = input.nextLine().split(" ");
 			
-			System.out.println("COMMAND: " + command);
+			for (String piece : command) {
+				System.out.println(piece);
+			}
+			
+			
+			// TODO Parse out commands into string[], like args[]
+			// look at the first command of each arg
+				// if unknown, respond with error
+			
+			OpResult result;
 			
 			// Parse command
-			if (command.equals("exit")) {
+			if (command[0].equals("exit")) {
 				runEngine = false;
 			}
+			else if (command[0].equals("create")) {
+				System.out.println("RESULT: " + controller.create(command));
+			}
+			
+			// Handle result
+			if
 		}
 		
 		input.close();
 		System.out.println("EXITING");
 	}
-
 }
