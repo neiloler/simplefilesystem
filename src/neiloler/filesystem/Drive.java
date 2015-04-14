40255b4166a70b1fef815a44a377ba45f0786a73
Neil Oler <neiloler@gmail.com>
2015-04-13 22:21:00 -0600

    Add Drive element.

---
+++ src/neiloler/filesystem/Drive.java
@@ -0,0 +1,38 @@
+package neiloler.filesystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Drive extends SimpleFile {
+
+	private List<SimpleFile> _files;
+	
+	public Drive(FileType fileType, String fileName, String filePath) {
+		_files = new ArrayList<>();
+		
+		_fileType = fileType;
+		_fileName = fileName;
+		_filePath = filePath;
+	}
+	
+	@Override
+	double getSize() {
+		double total = 0;
+		
+		for (SimpleFile simpleFile : _files) {
+			total += simpleFile.getSize();
+		}
+		
+		return total;
+	}
+
+	@Override
+	<T> T getContents() {
+		return (T) _files;
+	}
+	
+	public void addFile(SimpleFile file) {
+		_files.add(file);
+	}
+
+}
