1c5a4f98dd504631643d68712dbf9f94fe4dfdc2
Neil Oler <neiloler@gmail.com>
2015-04-15 00:07:43 -0600

    Remove ability to populate text at time of creation. (this isn't in the specs)

--- src/neiloler/filesystem/TextFile.java
+++ src/neiloler/filesystem/TextFile.java
@@ -4,11 +4,11 @@ public class TextFile extends SimpleFile {
 
 	private String _contents;
 	
-	public TextFile(String fileName, String filePath, String contents) {
+	public TextFile(String fileName, String filePath) {
 		_fileType = FileType.TextFile;
 		_fileName = fileName;
 		_filePath = filePath;
-		_contents = contents;
+		_contents = "";
 	}
 	
 	@Override
