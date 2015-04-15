01770a9feeed40c8ebadfb91106198b9ee62c496
Neil Oler <neiloler@gmail.com>
2015-04-14 22:26:36 -0600

    Tighten up constructors. This is a mild abuse of one generic object serving multiple needs, and this should be broken out into multiple objects at some point.

--- src/neiloler/filesystem/FileContainer.java
+++ src/neiloler/filesystem/FileContainer.java
@@ -7,9 +7,42 @@ import java.util.Map;
 public class FileContainer extends SimpleFile {
 
 	private Map<String, SimpleFile> _files;
+	private FileContainer _parent;
 	
-	public FileContainer(FileType fileType, String fileName, String filePath) {
+	/**
+	 * Create a drive object.
+	 * 
+	 * @param driveName Name of drive.
+	 */
+	public FileContainer(String driveName) {
+		_parent = null;
+		
 		_files = new HashMap<>();
+		
+		_fileType = FileType.Drive;
+		_fileName = driveName;
+		_filePath = driveName;
+	}
+	
+	/**
+	 * Create a folder or zip container
+	 * 
+	 * @param parent Parent FileContainer that contains this object
+	 * @param fileType The type of file
+	 * @param fileName
+	 * @param filePath
+	 * @throws Exception If you try to pass in a FileType that doesn't make sense (Drive or TextFile), you'll blow up
+	 */
+	// This is for folders and zips
+	public FileContainer(FileContainer parent, FileType fileType, String fileName, String filePath) throws Exception {
+		_files = new HashMap<>();
+		
+		_parent = parent;
+		
+		if (fileType == FileType.Drive || fileType == FileType.TextFile) {
+			// TODO THIS IS A PROBLEM, BARK AT USER/DEV TO NOT DO THIS, CREATE CUSTOM EXCEPTION
+			throw new Exception("Cannot instantiate a TextFile or a Drive this way!");
+		}
 		
 		_fileType = fileType;
 		_fileName = fileName;
@@ -39,5 +72,4 @@ public class FileContainer extends SimpleFile {
 	public void addFile(String fileName, SimpleFile file) {
 		_files.put(fileName, file);
 	}
-
 }
