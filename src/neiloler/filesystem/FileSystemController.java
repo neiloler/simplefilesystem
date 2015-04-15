926be314e22db2dbb3a2b9d36d6614802afe030b
Neil Oler <neiloler@gmail.com>
2015-04-15 00:08:25 -0600

    First draft of working file creation for a given path.

--- src/neiloler/filesystem/FileSystemController.java
+++ src/neiloler/filesystem/FileSystemController.java
@@ -2,13 +2,18 @@ package neiloler.filesystem;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 
 import neiloler.filesystem.SimpleFile.FileType;
 
 public class FileSystemController {
 	
-	List<FileContainer> _drives;
+	private Map<String, FileContainer> _drives;
+	private FileContainer _currentDrive;
 	
 	// SETUP HELPER
 	StringBuilder helpDisplay = new StringBuilder(
@@ -21,7 +26,12 @@ public class FileSystemController {
 			);
 	
 	public FileSystemController() {
-		_drives = new ArrayList<>();
+		_drives = new HashMap<>();
+		
+		// Add a default drive to start with
+		
+		_currentDrive = new FileContainer("default");
+		_drives.put(_currentDrive.getName(), _currentDrive);
 	}
 	
 	public enum OpResult {
@@ -49,7 +59,8 @@ public class FileSystemController {
 		else {
 			FileType fileType;
 			String fileName;
-			List<String> path;
+			String path;
+			List<String> pathTokens;
 			
 			// Get TYPE
 			if (command[1].equals("drive")) fileType = FileType.Drive;
@@ -69,19 +80,88 @@ public class FileSystemController {
 			// Get PATH
 			// TODO Do we need to protect against things like "//" (this will make a path level of "")
 			if (command.length == 3) {
-				path = new ArrayList<>();
-				path.add("");
+				// No path, should make in current path ('./')
+				pathTokens = new ArrayList<>();
+				pathTokens.add("");
+				path = "";
 			}
 			else {
-				path = new ArrayList<>();
+				path = command[3];
+				pathTokens = new ArrayList<>();
 				String[] pathArray = command[3].split("/");
-				path.addAll(Arrays.asList(pathArray));
+				pathTokens.addAll(Arrays.asList(pathArray));
 			}
 			
+			// Protect against making a drive inside anything else
+			if (fileType == FileType.Drive && !path.equals("")) {
+				return OpResult.FAILURE_ILLEGAL_FILE_SYSTEM_OPERATION;
+			}
 			
+			// We'll catch all Drive creation requests here and assume anything afterwards is NOT a Drive request 
+			if (fileType == FileType.Drive) {
+				_drives.put(fileName, new FileContainer(fileName));
+			}
 			
+			// Prep new file to be inserted in the specified place
+			if (fileType == FileType.TextFile) {
+				SimpleFile newFile;
+				newFile = new TextFile(fileName, path);
+				return createFileAtPath(newFile, _currentDrive, new LinkedList<String>(pathTokens));
+			}
+			else {
+				try {
+					SimpleFile newFile;
+					newFile = new FileContainer(fileType, fileName, path);
+					return createFileAtPath(newFile, _currentDrive, new LinkedList<String>(pathTokens));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
 		}
 		
 		return RESULT;
+	}
+	
+	private OpResult createFileAtPath(SimpleFile fileToCreate, FileContainer node, Queue<String> path) {
+		
+		// Stop - We've reached our destination
+		if (path.isEmpty()) {
+			// Do we have something named this already?
+			if (node.getContents().containsKey(fileToCreate.getName())) {
+				return OpResult.FAILURE_PATH_ALREADY_EXISTS;
+			}
+			else {
+				node.addFile(fileToCreate.getName(), fileToCreate);
+				return OpResult.SUCCESS;
+			}
+		}
+		
+		// Do we already have something named this?
+		if (node.getContents().containsKey(path.peek())) {
+			// YES - Something along this path already exists
+			if (node.getContents().get(path.peek()).getType() == FileType.TextFile) {
+				// We have been given a path that is treating this token as a container, but this already exists and is a TextFile. This is a no-no.
+				return OpResult.FAILURE_PATH_ALREADY_EXISTS;
+			}
+			else {
+				// We have another FileContainer to dive into
+				return createFileAtPath(fileToCreate, (FileContainer)node.getContents().get(path.poll()), path);
+			}
+			
+		}
+		else {
+			// NO - We need to make a folder that matches this token of the path
+			try {
+				String newFolderName = path.poll();
+				FileContainer newFolder;
+				newFolder = new FileContainer(FileType.Folder, newFolderName, node.getPath() + "/" + newFolderName);
+				node.getContents().put(newFolderName, newFolder);
+				return createFileAtPath(fileToCreate, newFolder, path);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return OpResult.FAILURE_ILLEGAL_FILE_SYSTEM_OPERATION;
 	}
 }
